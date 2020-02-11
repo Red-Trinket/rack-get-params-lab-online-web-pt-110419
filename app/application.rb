22f2f6a78a43a6648de1d1e9a1a4ba42462bad56
Matthew Jones <matts3371@gmail.com>
2020-02-11 00:49:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
         resp.write "Your cart is empty"
       end 
     elsif req.path.match(/add/)
-      item = req.params["q"]
+      item = req.params["item"]
       resp.write handle_add(item)
     else
       resp.write "Path Not Found"
